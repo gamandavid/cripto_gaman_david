4af04c8b369c9f9dd0edf4d9b4ab9a072c1df6bf
gaman.david988 <gaman.david988@gmail.com>
2023-01-08 20:02:07 +0200

    checkpoint 1

--- lab3/TheKeys.py
+++ lab3/TheKeys.py
@@ -1,3 +1,95 @@
+from Cryptodome.Util import number
+import random
+
+# XOR
+def byte_xor(ba1, ba2):
+    return bytes([a ^ b for a, b in zip(ba1, ba2)])
+
+
+# Folyamattikosito
+#def secreting(myalg, start, msg) :
+#    (mykey, start) = myalg(start)
+#    k = mykey.to_bytes(1,"big")
+
+#    secret = []
+#    for letter in msg :
+#        b = letter.to_bytes(1,"big")
+#        secret.append(byte_xor(b, k))
+
+#        (mykey, start) = myalg(start)
+#        k = mykey.to_bytes(1,"big")
+#    return secret
+
+
+def solitaire(kezd) :
+    return (253, kezd)
+
+
+# Incializalja a paklit Solitairhez veletlenszeruen
+def init_pakli() :
+    p = [i for i in range(54)]
+    random.shuffle(p)
+    return p
+
+# Meghivja az algoritmust es visszateriti a kulcsot
+def solitaire_256 ( kezd ) :
+    (kulcs1, kezd) = solitaire(kezd)
+    (kulcs2, kezd) = solitaire(kezd)
+    return ((kulcs1**3 + kulcs2**2)%256, kezd)
+
+# Solitaire
+def solitaire( kezd ) :
+    pakli = kezd.copy()
+    feher = 52
+    fekete = 53
+
+   # Feher dzsoker
+    feher_ind = pakli.index(feher) #megadja, hogy hol van az 52
+    kov = (feher_ind+1)%54
+    if kov==0 :
+        pakli.remove(feher)
+        pakli.insert(kov+1, feher)
+    else : 
+        pakli[feher_ind], pakli[kov] = pakli[kov], pakli[feher_ind]
+
+    #Fekete kettovel alatta
+    fekete_ind = pakli.index(fekete)
+    kov = (fekete_ind+2)%54
+    pakli.remove(fekete)
+    if kov<=1 :
+        pakli.insert(kov+1, fekete)
+    else :
+        pakli.insert(kov, fekete)
+
+    #Csere a dzsokereknel
+    ind1 = pakli.index(feher)
+    ind2 = pakli.index(fekete)
+
+    if ind1>ind2 :
+        ind1, ind2 = ind2, ind1
+    
+    elso = pakli[0:ind1]
+    masodik = pakli[ind1:ind2+1]
+    harmadik = pakli[ind2+1: 54]
+    pakli = harmadik + masodik + elso
+
+    #Torol also kartyak alapjan
+    also = pakli[53]
+    if also!=feher and also!=fekete:
+        bal = pakli[0 : also+1]
+        jobb = pakli[also+1 : 53]
+        pakli = jobb + bal + [also]
+
+    felso = pakli[0]
+    if felso == feher or felso == fekete :
+        return solitaire(pakli)
+    
+    return (pakli[felso+1], pakli)
+
+
+
+
+
 def generate_private_key(n=8):
     """Generate a private key for use in the Merkle-Hellman Knapsack Cryptosystem
 
@@ -78,3 +170,8 @@ def decrypt_mh(message, private_key):
     return message
 
 print('hello')    
+
+kezd = init_pakli()
+print(kezd)
+titkos = secreting(solitaire_256, kezd, "alma")
+print(titkos)
